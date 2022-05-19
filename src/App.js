fdb5ce36285777bde65c8136ae5286043f139f83
House of Giants <houseofgiants@Houses-MacBook-Air.local>
2022-05-18 23:43:31 -0700

    WIP

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data.js';
 
 function App() {
+  let lowScoringData = data.filter(question => question['percent_correct'] < 0.5)
+  // let highScoringData = data.filter(question => question['percent_correct'] > 0.5)
+
+function countRepeatedWords(sentence) {
+  let words = sentence.split(" ");
+  let wordMap = {};
+
+  for (let i = 0; i < words.length; i++) {
+    let currentWordCount = wordMap[words[i]];
+    let count = currentWordCount ? currentWordCount : 0;
+    wordMap[words[i]] = count + 1;
+  }
+  return wordMap;
+}
+let wordCountedArray = lowScoringData.map(question => (
+  question['text'] = countRepeatedWords(question['text'])
+));
+
+let newCountedArray = wordCountedArray.map((words => (words.flat()
+)))
+console.log(newCountedArray);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Hello World
       </header>
     </div>
   );
